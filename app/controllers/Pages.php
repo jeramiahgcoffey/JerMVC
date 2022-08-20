a26b3c2eb40a02980bce66cb20baef389e289b16
Jeramiah Coffey <jcoffey@rategenius.com>
2022-08-20 09:54:36 -0500

    Add header and footer includes

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -7,12 +7,13 @@ class Pages extends Controller
 
   public function index()
   {
-    $data = ['title' => 'Welcome'];
+    $data = ['title' => 'Welcome!'];
     $this->view('pages/index', $data);
   }
 
   public function about()
   {
-    $this->view('pages/about');
+    $data = ['title' => 'About Us'];
+    $this->view('pages/about', $data);
   }
 }
--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,1 +1,5 @@
-ABOUT
+<?php require APP_ROOT . '/views/inc/header.php'; ?>
+
+<h1><?php echo $data['title']; ?></h1>
+
+<?php require APP_ROOT . '/views/inc/footer.php'; ?>
